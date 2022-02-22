df6ff3f7a613d8f2769f65703713e07d2e9a0969
Grg2wenty <cw3141now@gmail.com>
2022-02-21 22:46:54 -0500

    Update Mv.java

--- A2/Assignment2/cscb07a2/commands/Mv.java
+++ A2/Assignment2/cscb07a2/commands/Mv.java
@@ -1,32 +1,4 @@
-// **********************************************************
-// Assignment2:
-// Student1:
-// UTORID user_name: wangj930
-// UT Student #: 1006390031
-// Author: Junheng Wang
-//
-// Student2:
-// UTORID user_name: anadaeth
-// UT Student #: 1003171907
-// Author: Ethan Ananda
-//
-// Student3:
-// UTORID user_name: shinhy22
-// UT Student #: 997743615
-// Author: Hyun Woo (Eddie) Shin
-//
-// Student4:
-// UTORID user_name: chouivan
-// UT Student #: 1003599732
-// Author: Ivanhoe Chou
-//
-//
-// Honor Code: I pledge that this program represents my own
-// program code and that I have coded on my own. I received
-// help from no one in designing and debugging my program.
-// I have also read the plagiarism section in the course info
-// sheet of CSC B07 and understand the consequences.
-// *********************************************************
+
 package commands;
 
 import java.util.Map;
@@ -38,11 +10,7 @@ import data.JFileManager;
 import errorHandler.NonvalidArgumentException;
 // TODO: Auto-generated Javadoc
 
-/**
- * The Class Mv.
- *
- * @author Junheng Wang, Hyun Woo (Eddie) Shin, Ivanhoe Chou, Ethan Anada
- */
+
 public class Mv implements Command {
 
   /**
